e393714499e0065d6ae5eb747c5dc5726fd800df
HardRain <christianmateus016@gmail.com>
2023-03-29 18:33:06 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace RE4_BIN_Util
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBINFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblBinFileName = new System.Windows.Forms.Label();
+            this.btnSplitModel = new System.Windows.Forms.Button();
+            this.btnMergeWith = new System.Windows.Forms.Button();
+            this.mainTable = new System.Windows.Forms.DataGridView();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(389, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBINFileToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.saveFileToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openBINFileToolStripMenuItem
+            // 
+            this.openBINFileToolStripMenuItem.Name = "openBINFileToolStripMenuItem";
+            this.openBINFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openBINFileToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.openBINFileToolStripMenuItem.Text = "Open BIN file";
+            this.openBINFileToolStripMenuItem.Click += new System.EventHandler(this.openBINFileToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(184, 6);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.saveFileToolStripMenuItem.Text = "Save file";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(184, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BIN Model Selected:";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.mainTable);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.lblBinFileName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 429);
+            this.panel1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.groupBox1.Controls.Add(this.btnMergeWith);
+            this.groupBox1.Controls.Add(this.btnSplitModel);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 128);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Panel";
+            // 
+            // lblBinFileName
+            // 
+            this.lblBinFileName.AutoSize = true;
+            this.lblBinFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBinFileName.ForeColor = System.Drawing.Color.White;
+            this.lblBinFileName.Location = new System.Drawing.Point(116, 21);
+            this.lblBinFileName.Name = "lblBinFileName";
+            this.lblBinFileName.Size = new System.Drawing.Size(19, 13);
+            this.lblBinFileName.TabIndex = 2;
+            this.lblBinFileName.Text = "---";
+            this.lblBinFileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSplitModel
+            // 
+            this.btnSplitModel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSplitModel.Enabled = false;
+            this.btnSplitModel.ForeColor = System.Drawing.Color.Black;
+            this.btnSplitModel.Location = new System.Drawing.Point(134, 30);
+            this.btnSplitModel.Name = "btnSplitModel";
+            this.btnSplitModel.Size = new System.Drawing.Size(121, 23);
+            this.btnSplitModel.TabIndex = 0;
+            this.btnSplitModel.Text = "Split BIN Model";
+            this.btnSplitModel.UseVisualStyleBackColor = true;
+            this.btnSplitModel.Click += new System.EventHandler(this.btnSplitModel_Click);
+            // 
+            // btnMergeWith
+            // 
+            this.btnMergeWith.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMergeWith.Enabled = false;
+            this.btnMergeWith.ForeColor = System.Drawing.Color.Black;
+            this.btnMergeWith.Location = new System.Drawing.Point(134, 75);
+            this.btnMergeWith.Name = "btnMergeWith";
+            this.btnMergeWith.Size = new System.Drawing.Size(121, 23);
+            this.btnMergeWith.TabIndex = 2;
+            this.btnMergeWith.Text = "Merge With...";
+            this.btnMergeWith.UseVisualStyleBackColor = true;
+            this.btnMergeWith.Click += new System.EventHandler(this.btnMergeWith_Click);
+            // 
+            // mainTable
+            // 
+            this.mainTable.AllowUserToAddRows = false;
+            this.mainTable.AllowUserToDeleteRows = false;
+            this.mainTable.AllowUserToResizeRows = false;
+            this.mainTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainTable.Location = new System.Drawing.Point(12, 194);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.Size = new System.Drawing.Size(365, 223);
+            this.mainTable.TabIndex = 4;
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(389, 456);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RE4 PS2 BIN Util";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBINFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblBinFileName;
+        private System.Windows.Forms.Button btnMergeWith;
+        private System.Windows.Forms.Button btnSplitModel;
+        private System.Windows.Forms.DataGridView mainTable;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RE4_BIN_Util
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+        string filepath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openBINFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dialog.Filter = "RE4 PS2 BIN Files (*.BIN)|*.BIN";
+            dialog.ShowDialog();
+            filepath = dialog.FileName;
+            if (filepath != "")
+            {
+                lblBinFileName.Text = Path.GetFileName(filepath);
+                btnMergeWith.Enabled = true;
+                btnSplitModel.Enabled = true;
+                ShowTextureTable(filepath);
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btnSplitModel_Click(object sender, EventArgs e)
+        {
+            BinaryReader br = new BinaryReader(File.Open(filepath, FileMode.Open));
+
+            // Get material offset
+            br.BaseStream.Position = 0x0A;
+            byte materialCount = br.ReadByte();
+
+            // Get material offset
+            br.BaseStream.Position = 0x0C;
+            uint materialsOffset = br.ReadUInt32();
+
+            // Get every material from the file
+            br.BaseStream.Position = materialsOffset;
+            List<byte[]> materialList = new List<byte[]>();
+
+            for (int material = 0; material < materialCount; material++)
+            {
+                materialList.Add(br.ReadBytes(0x10));
+            }
+
+            // Get every mesh
+            List<byte[]> meshes = new List<byte[]>();
+            List<byte[]> meshOffsets = new List<byte[]>();
+
+            // Get all mesh offsets
+            for (int i = 0; i < materialCount; i++)
+            {
+                br.BaseStream.Position = materialsOffset + 0x0C + (i * 0x10);
+                meshOffsets.Add(br.ReadBytes(0x04));
+            }
+
+            // Stores every mesh inside list
+            for (int material = 0; material < materialCount; material++)
+            {
+                br.BaseStream.Position = BitConverter.ToUInt32(meshOffsets[material], 0);
+                if (material != materialCount - 1)
+                {
+                    meshes.Add(br.ReadBytes((int)(BitConverter.ToUInt32(meshOffsets[material + 1], 0) - BitConverter.ToUInt32(meshOffsets[material], 0))));
+                }
+                else
+                {
+                    meshes.Add(br.ReadBytes((int)(br.BaseStream.Length - (int)BitConverter.ToUInt32(meshOffsets[material], 0))));
+                }
+            }
+
+            // Writes the files separately
+            Directory.CreateDirectory(Path.GetFileNameWithoutExtension(filepath));
+            for (int i = 0; i < materialCount; i++)
+            {
+                BinaryWriter bw = new BinaryWriter(
+                    File.Open($"{Path.GetFileNameWithoutExtension(filepath)}/" +
+                    $"{Path.GetFileNameWithoutExtension(filepath)}_mesh_{i}.MESH", FileMode.Create));
+
+                bw.Write(meshes[i]);
+                bw.Close();
+            }
+            MessageBox.Show($"Extracted {materialCount} meshes from loaded model.", "Information",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            br.Close();
+        }
+        private void btnMergeWith_Click(object sender, EventArgs e)
+        {
+            BinaryReader br = new BinaryReader(File.Open(filepath, FileMode.Open));
+
+            // Get material count
+            br.BaseStream.Position = 0x0A;
+            byte materialCount = br.ReadByte();
+
+            // Get material starting offset
+            br.BaseStream.Position = 0x0C;
+            uint materialsOffset = br.ReadUInt32();
+
+            // Splitting the file
+            br.BaseStream.Position = materialsOffset + 0x0C;
+            uint firstMeshOffset = br.ReadUInt32();
+
+            br.BaseStream.Position = 0;
+            byte[] topPart = br.ReadBytes((int)firstMeshOffset);
+            byte[] bottomPart = br.ReadBytes((int)(br.BaseStream.Length - firstMeshOffset));
+
+            br.Close();
+
+            dialog.Filter = "RE4 MESH Files (*.MESH)|*.MESH";
+            dialog.Multiselect = true;
+            dialog.ShowDialog();
+
+            // ----------------------------------
+            // Creates new custom merged Bin file
+            string mergedBinFilename = Path.GetFileNameWithoutExtension(filepath);
+            BinaryWriter bw = new BinaryWriter(File.Open($"{mergedBinFilename}_merged.bin", FileMode.Create));
+
+            // Writes data from start to end of material list
+            bw.Write(topPart);
+            uint tempAcumulator = 0;
+            for (int i = 0; i < dialog.FileNames.Length; i++)
+            {
+
+                bw.Write(new byte[] { 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF });
+                bw.Write((uint)(tempAcumulator + topPart.Length + bottomPart.Length));
+
+                BinaryReader mesh = new BinaryReader(File.Open(dialog.FileNames[i], FileMode.Open));
+                tempAcumulator += (uint)mesh.BaseStream.Length;
+
+                mesh.Close();
+            }
+
+            // Writes data after material list
+            bw.Write(bottomPart);
+            for (int i = 0; i < dialog.FileNames.Length; i++)
+            {
+                BinaryReader mesh = new BinaryReader(File.Open(dialog.FileNames[i], FileMode.Open));
+                bw.Write(mesh.ReadBytes((int)mesh.BaseStream.Length));
+                mesh.Close();
+            }
+            bw.Close();
+            UpdateMaterialsOffsets();
+
+            // Checks if user wants to open new created file
+            DialogResult result = MessageBox.Show($"Successfully merged {dialog.FileNames.Length} meshes and a new file has been created.\n\nDo you " +
+                $"want to open it?", "Information",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                filepath = $"{mergedBinFilename}_merged.bin";
+                ShowTextureTable($"{mergedBinFilename}_merged.bin");
+                lblBinFileName.Text = filepath;
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void UpdateMaterialsOffsets()
+        {
+            string mergedBinFilename = Path.GetFileNameWithoutExtension(filepath);
+            BinaryReader br = new BinaryReader(File.Open($"{mergedBinFilename}_merged.bin", FileMode.Open));
+
+            // Get material count
+            br.BaseStream.Position = 0x0A;
+            byte materialCount = br.ReadByte();
+
+            // Get material starting offset
+            br.BaseStream.Position = 0x0C;
+            uint materialsOffset = br.ReadUInt32();
+
+            // Splitting the file
+            br.BaseStream.Position = materialsOffset + 0x0C;
+            List<uint> pointerList = new List<uint>();
+
+            for (int i = 0; i < materialCount + dialog.FileNames.Length; i++)
+            {
+                pointerList.Add(br.ReadUInt32());
+                br.BaseStream.Position += 0x0C;
+            }
+
+            br.Close();
+
+            // WRITING DATA
+            BinaryWriter bw = new BinaryWriter(File.Open($"{mergedBinFilename}_merged.bin", FileMode.Open));
+
+            // Updates index
+            bw.BaseStream.Position = 0x0A;
+            bw.Write((byte)(materialCount + dialog.FileNames.Length));
+
+            // Updates material pointers
+            bw.BaseStream.Position = materialsOffset + 0x0C;
+            for (int i = 0; i < materialCount + dialog.FileNames.Length; i++)
+            {
+                bw.Write((uint)(pointerList[i] + (0x10 * dialog.FileNames.Length)));
+                bw.BaseStream.Position += 0x0C;
+            }
+            bw.Close();
+        }
+        private void ShowTextureTable(string file)
+        {
+            BinaryReader br = new BinaryReader(File.Open(file, FileMode.Open));
+
+            // Get material count
+            br.BaseStream.Position = 0x0A;
+            byte materialCount = br.ReadByte();
+
+            // Get material starting offset
+            br.BaseStream.Position = 0x0C;
+            uint materialsOffset = br.ReadUInt32();
+
+            // Get every texture index
+            List<byte> textureIndexList = new List<byte>();
+            br.BaseStream.Position = materialsOffset + 0x01;
+
+            for (int i = 0; i < materialCount; i++)
+            {
+                textureIndexList.Add(br.ReadByte());
+                br.BaseStream.Position += 0x0F;
+            }
+            br.Close();
+
+            // Create table
+            mainTable.Columns.Clear();
+            mainTable.Columns.Add("index", "Texture Index"); // 0
+            for (int i = 0; i < materialCount; i++)
+            {
+                var index = mainTable.Rows.Add();
+                mainTable.Rows[index].Cells[0].Value = textureIndexList[i];
+            }
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BinaryReader br = new BinaryReader(File.Open(filepath, FileMode.Open));
+
+                // Get material count
+                br.BaseStream.Position = 0x0A;
+                byte materialCount = br.ReadByte();
+
+                // Get material starting offset
+                br.BaseStream.Position = 0x0C;
+                uint materialsOffset = br.ReadUInt32();
+
+                br.Close();
+
+                BinaryWriter bw = new BinaryWriter(File.Open(filepath, FileMode.Open));
+                bw.BaseStream.Position = materialsOffset + 0x01;
+
+                for (int i = 0; i < materialCount; i++)
+                {
+                    if (Convert.ToInt16(mainTable.Rows[i].Cells[0].Value) > Byte.MaxValue)
+                    {
+                        MessageBox.Show($"Max byte value allowed is 255, wrong value found: {mainTable.Rows[i].Cells[0].Value}", "Error",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        bw.Close();
+                        return;
+                    }
+                    bw.Write(Convert.ToByte(mainTable.Rows[i].Cells[0].Value));
+                    bw.BaseStream.Position += 0x0F;
+                }
+                bw.Close();
+                MessageBox.Show($"File saved successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Could not save file, check any incorrect index values.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This tool was created for quick splitting and merging .bin model meshes," +
+                " example: extracting wesker glasses and merging them to leon face model.\n\n" +
+                "Note: the .mesh files generated with this tool cannot be viewed in a 3D editor naturally.\n" +
+                "Note 2: This tool is not recommended for begginers, as it's experimental and you must understand a bit about .bin file structure.\n\n" +
+                "Steps:\n" +
+                "1 - Open a bin file you want to extract the meshes or merge something to it;\n" +
+                "2 - Click on Split to extract all meshes from loaded model, or Merge With... to load .mesh files;\n" +
+                "3 - After merging, click on YES to load the new generated file with '_merged' suffix;\n" +
+                "4 - Update the texture indices accordingly to its .tpl texture order.",
+                "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
